2fbe796691d1166ca73bee829ed690b6ba4df3b6
PrattayDhar <93048656+PrattayDhar@users.noreply.github.com>
2022-06-24 16:16:04 +0100

    Add files via upload

---
+++ project/Decoder.java
@@ -0,0 +1,32 @@
+
+package algo.project;
+
+import java.util.HashMap;
+
+public class Decoder {
+
+    public String decodedStr;
+    public final HashMap<String, Character> hashMap;
+    public HashMap<String, Character> hmapCodeR;
+
+    public Decoder(String decodedStr, HashMap<String, Character> huffmanTree) {
+        this.decodedStr = decodedStr;
+        this.hashMap = huffmanTree;
+    }
+
+    public String decode() {
+        StringBuilder sb = new StringBuilder();
+        String t = "";
+
+        for (int i = 0; i < decodedStr.length(); i++) {
+            t += decodedStr.charAt(i);
+            if (hashMap.containsKey(t)) {
+                sb.append(hashMap.get(t));
+                t = "";
+            }
+        }
+        decodedStr = sb.toString();
+        return decodedStr;
+    }
+
+}
